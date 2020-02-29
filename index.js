fc59dce50185b815d3f2b603b91e3220a78a4c52
LIU <sqfkw1578@gmail.com>
2020-02-29 22:30:27 +0800

    疑?他的有東西

--- index.js
+++ index.js
@@ -1,63 +1,17 @@
-const express = require('express');
+var express    = require('express')
 var bodyParser = require('body-parser')
-const app = express();
 
-app.use(express.json());//加上一個middle ware，讓app使用
-app.use(bodyParser.json());
+var app = express()
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded())
 
+// parse application/json
+app.use(bodyParser.json())
 
-const Joi = require('joi');  //return a class
-//先在最上方把joi引進，會回傳一個class，所以這邊大寫
+app.listen(2000);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
-app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3, 4]);
-});
-
-app.get(' :year/:month', (req, res) => {
-  // res.send(req.params);
-  res.send(req.query);
-});
-
-const courses = [
-  { id: 1, name: 'course1' },
-  { id: 2, name: 'course2' },
-  { id: 3, name: 'course3' }
-];
-// app.get('/api/courses/:id', (req, res) => {
-//   let a = parseInt(req.params.id) * 2
-//   res.send(a.toString());
-// });
-
-// app.get('/api/courses/:id', (req, res) => {
-//   let id = req.params.id
-//   res.send(courses[id]);
-// });
-app.get('/api/courses/:id', (req, res) => {
-  let course = courses.find(
-    courses => courses.id === parseInt(req.params.id)
-  );
-  if (!course) {
-    res.status(404).send('The course with the given ID was not found');
-    return;
-  }
-  res.send(course);
-});
-
-app.post('/c', function (req, res) {
-  console.log(req.body)
-  
-  res.send('POST request to homepage')
-})
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+app.post("/", function (req, res) {
+  console.log(req.body) // populated!
+  res.send(200, req.body.name);
 });
