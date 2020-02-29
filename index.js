036b4a2d06e45e91a8e87a3d3f1454081d46512e
LIU <sqfkw1578@gmail.com>
2020-02-29 22:28:16 +0800

    如何POST都無法

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
+var bodyParser = require('body-parser')
 const app = express();
+
+app.use(express.json());//加上一個middle ware，讓app使用
+app.use(bodyParser.json());
+
+
+
+const Joi = require('joi');  //return a class
+//先在最上方把joi引進，會回傳一個class，所以這邊大寫
+
+
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -32,8 +43,19 @@ app.get('/api/courses/:id', (req, res) => {
   let course = courses.find(
     courses => courses.id === parseInt(req.params.id)
   );
+  if (!course) {
+    res.status(404).send('The course with the given ID was not found');
+    return;
+  }
   res.send(course);
 });
+
+app.post('/c', function (req, res) {
+  console.log(req.body)
+  
+  res.send('POST request to homepage')
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
