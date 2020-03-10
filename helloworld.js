7e1cb61a36d104128d027bdc44833dba07b7ea59
LIU <sqfkw1578@gmail.com>
2020-03-10 13:12:14 +0800

    打算使用CORS套件

--- helloworld.js
+++ helloworld.js
@@ -2,18 +2,32 @@ console.clear()
 const express = require("express")
 const app = express()
 
-app.all('/', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  // res.header("Access-Control-Allow-Headers", "X-Requested-With")//"X-Requested-With"
-  // res.header("Access-Control-Allow-Methods", "GET")
-  // Access-Control-Allow-Credentials
+// app.all('/', function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")//"X-Requested-With"
+//   res.header("Access-Control-Allow-Credentials", true)
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With, origin, content-type, accept")
+//   // res.header("Access-Control-Allow-Methods", "GET")
+//   next()
+// })
+app.use((req, res, next) => {
+  console.log(req.headers)
   next()
 })
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+app.options(
+
+)
 app.get('/', (req, res) => {
   // console.log(req.headers)
   // console.log(res.headers)
@@ -25,7 +39,7 @@ app.post('/', function (req, res) {
 app.put('/user', function (req, res) {
   res.send('Got a PUT request at /user');
 });
-app.delete('/user', function (req, res) {
+app.delete('/', function (req, res) {
   res.send('Got a DELETE request at /user');
 });
 app.listen(3000, () => { console.log("Example app listening on port 3000!") })
