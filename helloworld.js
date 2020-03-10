f1db3f2cbda4317ca9d00e927441edc0ab92cc72
LIU <sqfkw1578@gmail.com>
2020-03-10 14:18:26 +0800

    依舊無法cors delet put

--- helloworld.js
+++ helloworld.js
@@ -1,45 +1,40 @@
 console.clear()
-const express = require("express")
-const app = express()
+var express = require("express")
+var cors = require('cors')
+// const referrerPolicy = require('referrer-policy')
+var app = express()
 
 // app.all('/', function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")//"X-Requested-With"
 //   res.header("Access-Control-Allow-Credentials", true)
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With, origin, content-type, accept")
-//   // res.header("Access-Control-Allow-Methods", "GET")
+//   res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS")
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+//   // res,header("Referrer-Policy","origin")
 //   next()
 // })
-app.use((req, res, next) => {
-  console.log(req.headers)
-  next()
-})
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
 
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-app.options(
+const corsOptions = {
+  origin: [
+    'http://localhost:5500',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-)
+app.use(cors(corsOptions));
+
+app.options('/products/:id', cors()) 
+
 app.get('/', (req, res) => {
-  // console.log(req.headers)
-  // console.log(res.headers)
   res.send("Hello World!")
 })
 app.post('/', function (req, res) {
   res.send('Got a POST request');
-});
-app.put('/user', function (req, res) {
+})
+app.put('/', function (req, res) {
   res.send('Got a PUT request at /user');
-});
+})
 app.delete('/', function (req, res) {
   res.send('Got a DELETE request at /user');
-});
+})
 app.listen(3000, () => { console.log("Example app listening on port 3000!") })
