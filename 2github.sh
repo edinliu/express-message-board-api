0483d41e7a84d850cc1853344d95aaa4bfcde727
LIU <sqfkw1578@gmail.com>
2020-04-14 17:47:09 +0800

    移除資料

--- 2github.sh
+++ 2github.sh
@@ -1,2 +1,2 @@
-git remote add github https://github.com/syyui15926/express-message-board-api.git
+# git remote add github https://github.com/syyui15926/express-message-board-api.git
 git push -u github master
